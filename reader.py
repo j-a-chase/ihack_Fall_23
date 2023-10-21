ca8df0db6517da3c2938ed5f898d987420334c23
James Chase <gochases@gmail.com>
2023-10-20 23:46:56 -0600

    Add get_past_assignments function

--- reader.py
+++ reader.py
@@ -18,7 +18,7 @@ class Reader:
         # base link to API
         self.api = "https://byui.instructure.com/api/v1/"
 
-        # holds user token
+        # holds user token (user_id=622529)
         self.token = token
 
         # holds course information for the student
@@ -31,7 +31,11 @@ class Reader:
         self.get_courses()
 
         # add upcoming assignment info for each course
-        for cid in self.ids: self.get_upcoming_assignments(cid)
+        # for cid in self.ids: self.get_upcoming_assignments(cid)
+
+        # grab graded assignments
+        # self.get_past_assignments(self.ids[0], 14)
+        for cid in self.ids: self.get_past_assignments(cid, 14)
 
     def get_courses(self) -> None:
         '''
@@ -61,7 +65,8 @@ class Reader:
                     x['enrollments'][0]['computed_current_score'],
                     x['enrollments'][0]['computed_current_grade'],
                     {
-                        'upcoming_assignments': []
+                        'upcoming_assignments': [],
+                        'past_assignments': []
                     }
                 ]
             # should be impossible at this point but just in case
@@ -70,10 +75,9 @@ class Reader:
         self.ids = list(self.courses.keys())
 
     def get_upcoming_assignments(self, course_id: str) -> None:
-        data = requests.get(self.api + f'courses/{course_id}/assignments?include=submission&include=score_statistics&bucket=upcoming&order_by=due_at&access_token={self.token}').text
+        data = requests.get(self.api + f'courses/{course_id}/assignments?&bucket=upcoming&order_by=due_at&access_token={self.token}').text
         parsed = json.loads(data)
 
-        self.courses[course_id]
         for x in parsed:
             # convert due date to current timezone
             due_date = datetime.strptime(x['due_at'], f'%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc).astimezone(tz=None)
@@ -82,5 +86,43 @@ class Reader:
 
             # append the assignment to the upcoming assignments dictionary inside the course
             self.courses[course_id][-1]['upcoming_assignments'].append([x['name'], due_date, x['html_url']])
+
+    def get_past_assignments(self, course_id: str, days_back: int) -> None:
+        def leap_year(year: int) -> bool:
+            return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+        
+        def get_month_days(m: int, y: int) -> int:
+            if m in {1,3,5,7,8,10,12}: return 31
+            elif m == 2: return 29 if leap_year(y) else 28
+            else: return 30
+
+
+        data = requests.get(self.api + f'courses/{course_id}/assignments?include[]=submission&include[]=score_statistics&bucket=past&per_page=100&order_by=due_at&access_token={self.token}').text
+        parsed = json.loads(data)
+
+        this_year = datetime.now().year
+        this_month = datetime.now().month
+        this_day = datetime.now().day
+
+        if this_day < days_back:
+            this_month -= 1
+            if this_month == 0: this_month = 12
+            this_day += get_month_days(this_month, this_year)
+            
+        for x in parsed:
+            due_date = datetime.strptime(x['due_at'], f'%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc).astimezone(tz=None)
+            d = due_date.day
+            if this_month != due_date.month or this_day - d > days_back: continue
+            try:
+                self.courses[course_id][-1]['past_assignments'].append([x['name'],
+                                                                        x['html_url'],
+                                                                        x['submission']['score'],
+                                                                        x['points_possible'],
+                                                                        x['score_statistics']['mean']])
+            except KeyError:
+                self.courses[course_id][-1]['past_assignments'].append([x['name'],
+                                                                        x['html_url'],
+                                                                        x['submission']['score'],
+                                                                        x['points_possible']])
 
 if __name__ == '__main__': assert False, 'This is a class file. Please import into another file.'
