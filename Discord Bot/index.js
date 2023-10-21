1eb3c9469294c759bf4d79f3b4811c6b9b94a8e4
mwhepworth1 <hep21001@byui.edu>
2023-10-20 20:04:58 -0600

    Created Guild/DM message detection

--- Discord Bot/index.js
+++ Discord Bot/index.js
@@ -1,4 +1,4 @@
-import { Client, EmbedBuilder, GatewayIntentBits } from 'discord.js';
+import { Client, EmbedBuilder, GatewayIntentBits, Partials } from 'discord.js';
 import config from "./settings.json" assert { type: "json"};
 
 const client = new Client({ 
@@ -6,39 +6,60 @@ const client = new Client({
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers
-    ] 
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.DirectMessages
+    ],
+    partials: [
+        Partials.Channel,
+        Partials.Message
+    ]
 });
 
 //Embeds:
-const helpEmbed = new EmbedBuilder()
-    .setColor(0xE80231)
-    .setTitle('Canvas LMS - BYUI')
-    .setDescription('See a list of commands and valid arguments.')
-    .addFields(
-        { name: '`help`', value: 'Displays this menu.'}
-    )
-    .setTimestamp()
-    .setFooter
+const helpEmbed = {
+    color: 0xE80231,
+    title: 'Canvas LMS - BYUI',
+    description: 'See a list of commands and valid arguments.',
+    thumbnail: {
+        url: config.logoURL,
+    },
+    fields: [
+        { 
+            name: '`help`', 
+            value: 'Displays this menu.'
+        }
+    ],
+    timestamp: new Date().toISOString(),
+    footer: {
+        text: 'A BYU-Idaho Hackathon Project',
+        iconURL: config.logoURL,
+    },
+};
+    
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('messageCreate', message => {
-  if (message.content.startsWith(config.prefix)) {
-    const prefix = message.content.slice(1);
-    const command = prefix.split(' ')[0];
+    if (message.author.bot == true) return;
+    if (message.guildId != null) {
+        if (message.content.startsWith(config.prefix)) {
+            const prefix = message.content.slice(1);
+            const command = prefix.split(' ')[0];
 
-    switch (command) {
-      case 'ping':
-        message.channel.send('pong');
-        break;
-      case 'help':
-        message.channel.send({embeds: helpEmbed});
-        break;
-    }
-  }
+            switch (command) {
+            case 'ping':
+                message.channel.send('pong');
+                break;
+            case 'help':
+                message.channel.send({embeds: [helpEmbed]});
+                break;
+            }
+        }
+    } else if (message.guildId == null) {
+        message.channel.send('Yay!')
+    } else return;
 });
 
 client.login(config.token);
