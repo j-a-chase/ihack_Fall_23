c08ba340ebbc1b6e91c6b2a87d7e79a0a747b3aa
mwhepworth1 <hep21001@byui.edu>
2023-10-20 23:37:42 -0600

    refined settings menu and user settings

--- Discord Bot/addons/messages.js
+++ Discord Bot/addons/messages.js
@@ -1,9 +1,9 @@
 const config = require("../settings.json"),
-      userSettings = require("./user-settings.js");
+      {userSettings} = require("./user-settings.js");
 const embedMSG = {
     guilds: {
         helpEmbed: {
-            color: parseInt(config.color),
+            color: userSettings.embedColor,
             title: 'Canvas LMS - BYUI',
             description: 'See a list of commands and valid arguments.',
             thumbnail: {
@@ -24,7 +24,7 @@ const embedMSG = {
     },
     direct: {
         helpEmbed: {
-            color: parseInt(config.color),
+            color: userSettings.embedColor,
             title: 'Canvas LMS - BYUI - DM',
             description: 'See a list of commands and valid arguments.',
             thumbnail: {
@@ -45,36 +45,36 @@ const embedMSG = {
     },
     settings: {
         mainEmbed: {
-            color: parseInt(config.color),
+            color: userSettings.embedColor,
             title: 'Canvas LMS | Settings ',
-            description: 'A list of current settings. `null` represents no settings defined.',
+            description: 'A list of current settings. " *(default)* " represents no user defined settings.',
             thumbnail: {
                 url: config.logoURL,
             },
             fields: [
                 { 
                     name: '**API Key**', 
-                    value: '*This content is not displayed for security purposes.*',
+                    value: `${userSettings.apiKey}`,
                     inline: false,
                 },
                 {
                     name: '**Embed Color**',
-                    value: `\`${userSettings.color}\``,
+                    value: `${userSettings.color.toString(16)}`,
                     inline: true,
                 },
                 {
                     name: '**Show Assignment Links**',
-                    value: `\`${userSettings.links}\``,
+                    value: `${userSettings.links}`,
                     inline: true,
                 }, 
                 {
                     name: '**Show Grades**',
-                    value: `\`${userSettings.grades}\``,
+                    value: `${userSettings.grades}`,
                     inline: true,
                 },
                 {
                     name: '**Display Recent Grades**',
-                    value: `from \`${userSettings.recentGrades}\` days ago *(default)*`
+                    value: `from the last ${userSettings.recentGrades}`
                 }
             ],
             timestamp: new Date().toISOString(),
@@ -84,7 +84,7 @@ const embedMSG = {
             },
         },
         changeSetting: {
-            color: parseInt(config.color),
+            color: userSettings.embedColor,
             title: 'Canvas LMS | Settings Error',
             description: 'No setting was defined. Available values are:\n\n`key` > Set the API Key\n`color` > Set the Message Embed Color\n`links` > Show/Hide Assignment Links\n`grades` > Show/Hide Grades',
             thumbnail: {
@@ -97,7 +97,7 @@ const embedMSG = {
             },
         },
         changeKey: {
-            color: parseInt(config.color),
+            color: userSettings.embedColor,
             title: 'Canvas LMS | Error Changing API Key',
             description: 'Not enough arguments. Make sure to include your API Key.',
             thumbnail: {
@@ -110,7 +110,7 @@ const embedMSG = {
             },
         },
         setKey: {
-            color: parseInt(config.color),
+            color: userSettings.embedColor,
             title: 'Canvas LMS | Changed API Key',
             description: 'Successfully changed API Key! Please allow a few minutes for this change to take effect.',
             thumbnail: {
@@ -123,7 +123,7 @@ const embedMSG = {
             },
         },
         changeColor: {
-            color: parseInt(config.color),
+            color: userSettings.embedColor,
             title: 'Canvas LMS | Error Changing Embed Color',
             description: 'Not enough arguments. Make sure to include your color as a HEX code.',
             thumbnail: {
@@ -136,7 +136,7 @@ const embedMSG = {
             },
         },
         setColor: {
-            color: parseInt(config.color),
+            color: userSettings.embedColor,
             title: 'Canvas LMS | Changed Embed Color',
             description: 'Successfully changed Embed Color! Please allow a minute for this change to take effect.',
             thumbnail: {
@@ -149,7 +149,7 @@ const embedMSG = {
             },
         },
         changeLinks: {
-            color: parseInt(config.color),
+            color: userSettings.embedColor,
             title: 'Canvas LMS | Error Changing Link Display Settings',
             description: 'Not enough arguments. Make sure to include `show` or `hide`.',
             thumbnail: {
@@ -162,7 +162,7 @@ const embedMSG = {
             },
         },
         setShowLinks: {
-            color: parseInt(config.color),
+            color: userSettings.embedColor,
             title: 'Canvas LMS | Showing Assignment Links',
             description: 'Successfully changed the display of assignment links to `show`! Please allow a few minutes for this change to take effect.',
             thumbnail: {
@@ -175,7 +175,7 @@ const embedMSG = {
             },
         },
         setHideLinks: {
-            color: parseInt(config.color),
+            color: userSettings.embedColor,
             title: 'Canvas LMS | Hiding Assignment Links',
             description: 'Successfully changed the display of assignment links to `hide`! Please allow a few minutes for this change to take effect.',
             thumbnail: {
@@ -188,7 +188,7 @@ const embedMSG = {
             },
         },
         invalidLinkSetting: {
-            color: parseInt(config.color),
+            color: userSettings.embedColor,
             title: 'Canvas LMS | Error Changing Display of Assignment Links',
             description: 'Please indicate if you\'d like to `hide` or `show` links to your assignments in messages.',
             thumbnail: {
@@ -201,7 +201,7 @@ const embedMSG = {
             },
         },
         changeGrades: {
-            color: parseInt(config.color),
+            color: userSettings.embedColor,
             title: 'Canvas LMS | Error Changing Grade Display Settings',
             description: 'Not enough arguments. Make sure to include `show` or `hide`.',
             thumbnail: {
@@ -214,7 +214,7 @@ const embedMSG = {
             },
         },
         setShowGrades: {
-            color: parseInt(config.color),
+            color: userSettings.embedColor,
             title: 'Canvas LMS | Showing Assignment Grades',
             description: 'Successfully changed the display of assignment grades to `show`! Please allow a few minutes for this change to take effect.',
             thumbnail: {
@@ -227,7 +227,7 @@ const embedMSG = {
             },
         },
         setHideGrades: {
-            color: parseInt(config.color),
+            color: userSettings.embedColor,
             title: 'Canvas LMS | Hiding Assignment Grades',
             description: 'Successfully changed the display of assignment grades to `hide`! Please allow a few minutes for this change to take effect.',
             thumbnail: {
@@ -240,7 +240,7 @@ const embedMSG = {
             },
         },
         invalidGradeSetting: {
-            color: parseInt(config.color),
+            color: userSettings.embedColor,
             title: 'Canvas LMS | Error Changing Display of Assignment Grades',
             description: 'Please indicate if you\'d like to `hide` or `show` grades for your assignments in messages.',
             thumbnail: {
--- Discord Bot/addons/user-settings.js
+++ Discord Bot/addons/user-settings.js
@@ -1,15 +1,16 @@
 
 //Default Values
-let color = parseInt('0xE80231'),
-    links = "YES",
-    grades = "YES",
+let color = "`0xE80231`",
+    links = "`YES`",
+    grades = "`YES`",
     recentGrades = 14,
-    defaultStr = " *(default)*"
+    defaultStr = " *(default)*",
+    apiKey = '*This content is not displayed for security purposes.*'
 
 let defaults = {
-    color: parseInt('0xE80231'),
-    links: "YES",
-    grades: "YES",
+    color: "`0xE80231`",
+    links: "`YES`",
+    grades: "`YES`",
     recentGrades: 14,
 }
 
@@ -21,19 +22,20 @@ let isDefault = {
 }
 
 //DATABSE CALLS
-
+updateDefaults();
 async function updateDefaults() {
     if (defaults.color == color) isDefault.color = defaultStr;
-    if (defaults.links == links) defaults.links = defaultStr;
+    if (defaults.links == links) isDefault.links = defaultStr;
     if (defaults.grades == grades) isDefault.grades = defaultStr;
-    if (defaults.recentGrades == recentGrades) defaults.recentGrades = defaultStr;
+    if (defaults.recentGrades == recentGrades) isDefault.recentGrades = " "+defaultStr;
 }
 
 const userSettings = {
-    embedColor: color,
+    embedColor: parseInt(color.split("`")[1]),
     color: color + isDefault.color,
     links: links + isDefault.links,
     grades: grades + isDefault.grades,
-    recentGrades: recentGrades + isDefault.recentGrades,
+    recentGrades: "`"+recentGrades+"`" + " days" + isDefault.recentGrades,
+    apiKey: apiKey,
 }
-module.exports = {userSettings};
+module.exports = { userSettings };
