5ae208e1b0cfd1e66c05847877c3daf3f7434e8d
James Chase <gochases@gmail.com>
2023-10-20 18:27:03 -0600

    Add reader.py file

---
+++ reader.py
@@ -0,0 +1,22 @@
+######################################################################################################################################################
+# Name: James A. Chase
+# File: reader.py
+# Date: 20 October 2023
+# Description:
+#
+# Class file for the Reader class.
+#
+######################################################################################################################################################
+
+# imports
+import requests
+import json
+
+class Reader:
+    def __init__(self):
+        self.api = "https://byui.instructure.com/api/v1/"
+
+    def get_courses(self, user_token: str):
+        pass
+
+if __name__ == '__main__': assert False, 'This is a class file. Please import into another file.'
